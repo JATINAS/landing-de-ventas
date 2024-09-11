947449b2bbbdedf05fcb1ce5ab40347e799da88f
JATINAS <jairpc385@gmail.com>
2024-09-11 13:52:21 -0600

    add new file

---
+++ functions.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    alert("Message sended, our employees will contact to you on few moments.");
+  }
+  function myFunction2(target) {
+    alert("Your email was sended.");
+    target.value="";
+  }
+  function goToPartner() {
+      window.location.href = "http://www.w3schools.com";
+  }
+  function goToCertificates() {
+      window.location.href = "https://www.ternbicycles.com/en/explore/product-safety/e-bike-certifications-ones-really-matter-your-safety";
+  }
